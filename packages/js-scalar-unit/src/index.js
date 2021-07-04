de0ecf0db7361821e1a8516077e35b68a4980ce0
Kether Saturnius <kether@primacypublishing.com>
2021-07-04 12:59:20 +1000

    feat(packages): re-implemented js-scalar-unit plugin

---
+++ packages/js-scalar-unit/src/index.js
@@ -0,0 +1,41 @@
+function getScrollbarWidth() {
+  return window.innerWidth - document.documentElement.clientWidth
+}
+
+export const jsScalarUnit = {
+  init: function () {
+    const root = getComputedStyle(document.documentElement)
+
+    function scalarCalculator() {
+      const start = root.getPropertyValue('--screenStartSize').trim()
+      const end = root.getPropertyValue('--screenEndSize').trim()
+
+      if (window.innerWidth < end) {
+        const min = root
+          .getPropertyValue('--screenMinFontSize')
+          .trim()
+          .replace('%', '')
+        document.documentElement.style.setProperty(
+          '--screenScalarFontSize',
+          `${min / (start / window.innerWidth)}%`,
+        )
+      }
+
+      console.log('INIT JS SCALAR')
+
+      document.documentElement.style.setProperty(
+        '--scrollbarWidth',
+        `${getScrollbarWidth()}px`,
+      )
+
+      document.documentElement.style.setProperty(
+        '--windowWidth',
+        `${window.innerWidth - getScrollbarWidth()}px`,
+      )
+    }
+
+    scalarCalculator()
+
+    window.addEventListener('resize', scalarCalculator)
+  },
+}
