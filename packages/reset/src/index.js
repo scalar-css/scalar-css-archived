864062e5b3c1be6b6a1ade224915c8d13727f035
Kether Saturnius <kether@primacypublishing.com>
2021-07-04 13:25:48 +1000

    feat(packages): migrated reset to be tailwind plugin instead

---
+++ packages/reset/src/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const pkg = require('../package.json')
+
+module.exports = function reset({ addBase, postcss }) {
+  const resetStyles = postcss.parse(
+    fs.readFileSync(`${__dirname}/reset.css`, 'utf8'),
+  )
+
+  addBase([
+    postcss.comment({
+      text: `! ScalarCSS v${pkg.version} | MIT License | https://scalar-css.com`,
+    }),
+    ...resetStyles.nodes,
+  ])
+}
