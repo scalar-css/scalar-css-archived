9f39dd47f09fe8ceab3e5505e31057ef0fb67687
Kether Saturnius <iamkether@gmail.com>
2021-07-13 20:19:51 +1000

    wip(packages): made good progress on getting font size changing properly again

---
+++ packages/screens/src/defaults.js
@@ -0,0 +1,70 @@
+// props to https://www.modularscale.com
+module.exports.defaultFontScales = {
+  minorSecond: 1.067,
+  majorSecond: 1.125,
+  minorThird: 1.2,
+  majorThird: 1.25,
+  perfectFourth: 1.333,
+  augmentedFourth: 1.414,
+  perfectFifth: 1.5,
+  minorSixth: 1.6,
+  goldenRatio: 1.618,
+  majorSixth: 1.667,
+  minorSeventh: 1.778,
+  majorSeventh: 1.875,
+  octave: 2,
+  majorTenth: 2.5,
+  majorEleventh: 2.667,
+  majorTwelfth: 3,
+  doubleOctave: 4,
+}
+
+// CSS Modern Font Stacks
+// https://gist.github.com/don1138/5761014
+module.exports.fontStacks = {
+  system: {
+    fontFamily:
+      'system, -apple-system, ".SFNSText-Regular", "San Francisco", "Roboto", "Segoe UI", "Helvetica Neue", "Lucida Grande", sans-serif',
+    capHeight: 0.7,
+  },
+  times: {
+    fontFamily:
+      'Cambria, "Hoefler Text", Utopia, "Liberation Serif", "Nimbus Roman No9 L Regular", Times, "Times New Roman", serif',
+    capHeight: 0.7,
+  },
+  georgia: {
+    fontFamily:
+      'Constantia, "Lucida Bright", Lucidabright, "Lucida Serif", Lucida, "DejaVu Serif", "Bitstream Vera Serif", "Liberation Serif", Georgia, serif',
+    capHeight: 0.7,
+  },
+  garamond: {
+    fontFamily:
+      '"Palatino Linotype", Palatino, Palladio, "URW Palladio L", "Book Antiqua", Baskerville, "Bookman Old Style", "Bitstream Charter", "Nimbus Roman No9 L", Garamond, "Apple Garamond", "ITC Garamond Narrow", "New Century Schoolbook", "Century Schoolbook", "Century Schoolbook L", Georgia, serif',
+    capHeight: 0.7,
+  },
+  helvetica: {
+    fontFamily:
+      'Frutiger, "Frutiger Linotype", Univers, Calibri, "Gill Sans", "Gill Sans MT", "Myriad Pro", Myriad, "DejaVu Sans Condensed", "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    capHeight: 0.7,
+  },
+  verdana: {
+    fontFamily:
+      'Corbel, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, "Verdana Ref", sans-serif',
+    capHeight: 0.7,
+  },
+  trebuchet: {
+    fontFamily:
+      '"Segoe UI", Candara, "Bitstream Vera Sans", "DejaVu Sans", "Bitstream Vera Sans", "Trebuchet MS", Verdana, "Verdana Ref", sans-serif',
+    capHeight: 0.7,
+  },
+  impact: {
+    fontFamily:
+      'Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans-serif',
+    capHeight: 0.7,
+  },
+  monospace: {
+    fontFamily:
+      'Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace',
+    capHeight: 0.7,
+  },
+}
--- packages/screens/src/index.js
+++ packages/screens/src/index.js
@@ -1,34 +1,84 @@
+const {
+  setBaseFontSizePx,
+  setBaseLineHeight,
+  setVerticalRhythm,
+  setBreakpointStartPx,
+  setBreakpointEndPx,
+  setFontScale,
+  calculateRootFontSize,
+} = require('./setup')
+
+/**
+ * Finalize the screen properties by duplicating/merging a few values
+ * to allow easier access to properties for calculation purposes in
+ * other areas of the framework. If a screen does not have new/existing
+ * values set, then we re-use the values from the previous screen.
+ *
+ * @param {Object} config
+ * @returns {Object}
+ */
 module.exports = function screens({ addComponents, theme }) {
   const screens = theme('screens', {})
   const screenList = Object.entries(screens)
-  const screenStyles = screenList.map(([key, value], index) => {
-    const boundary =
-      index + 1 < screenList.length
-        ? screenList[index + 1][1]
-        : screenList[index - 1][1]
+  const screenStyles = screenList.map(([key, screen], index) => {
+    const nextScreen =
+      index + 1 < screenList.length ? screenList[index + 1][1] : null
+    const prevScreen = index !== 0 ? screenList[index - 1][1] : null
+
+    // finalize screen properties by duplicating/merging a few values
+    screen.baseFontSizePx = setBaseFontSizePx(screen, prevScreen)
+    screen.baseLineHeight = setBaseLineHeight(screen, prevScreen)
+    screen.verticalRhythm = setVerticalRhythm(screen, prevScreen)
+    screen.breakpointStartPx = setBreakpointStartPx(screen, prevScreen)
+    screen.breakpointEndPx = setBreakpointEndPx(screen, nextScreen)
+    screen.fontScale = setFontScale(screen, prevScreen)
+    screen.baseLineHeightPx = screen.baseLineHeight * screen.baseFontSizePx
+    screen.verticalRhythmPx = screen.verticalRhythm * screen.baseFontSizePx
+
+    const { screenMinFontSize, screenMaxFontSize } = calculateRootFontSize(
+      screen,
+      prevScreen,
+    )
+
+    const fontProperties = {
+      '--screen-min-font-size': `${screenMinFontSize}%`,
+      '--screen-scalar-font-size': `${screenMinFontSize}%`,
+      '--screen-max-font-size': `${screenMaxFontSize}%`,
+      '--screen-line-height': `${screen.baseLineHeight}`,
+      '--screen-rhythm': `${screen.verticalRhythm}`,
+    }
 
     if (key === 'start') {
       return {
         ':root': {
-          '--screen-start-size': value.min,
-          '--screen-end-size': boundary.min,
+          '--screen-start-size': screen.min,
+          '--screen-end-size': nextScreen.min,
+          ...fontProperties,
+        },
+        html: {
+          'line-height': 'var(--screen-line-height)',
+          'font-size':
+            'clamp(var(--screen-min-font-size), var(--screen-scalar-font-size, var(--screen-min-font-size)), var(--screen-max-font-size))',
         },
       }
     } else if (key === 'end') {
       return {
-        [`@media (min-width: ${boundary.min}) and (max-width: ${value.max})`]: {
-          ':root': {
-            '--screen-start-size': boundary.min,
-            '--screen-end-size': value.max,
+        [`@media (min-width: ${prevScreen.max}) and (max-width: ${screen.max})`]:
+          {
+            ':root': {
+              '--screen-start-size': prevScreen.max,
+              '--screen-end-size': screen.max,
+              ...fontProperties,
+            },
           },
-        },
       }
     } else {
       return {
-        [`@media (min-width: ${value.min})`]: {
+        [`@media (min-width: ${screen.min})`]: {
           ':root': {
-            '--screen-start-size': value.min,
-            '--screen-end-size': boundary.min,
+            '--screen-start-size': screen.min,
+            '--screen-end-size': nextScreen.min,
+            ...fontProperties,
           },
         },
       }
