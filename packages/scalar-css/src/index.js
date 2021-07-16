390dc1dfeaf480d8c9ac9bf827ff49eba3857c75
Kether Saturnius <iamkether@gmail.com>
2021-07-16 18:59:50 +1000

    feat(packages): re-implemented scalar css reset as normal postcss plugin

--- packages/scalar-css/src/index.js
+++ packages/scalar-css/src/index.js
@@ -1,12 +1,16 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import setup from './lib/setup'
+import reset from './lib/plugins/reset'
 
 module.exports = (config = {}) => {
-	const ctx = setup(config)
+  const ctx = setup(config)
 
-	return {
-		postcssPlugin: 'scalarcss',
-		plugins: [setup(config)],
-	}
+  return {
+    postcssPlugin: 'scalarcss',
+    plugins: [reset(ctx)],
+  }
 }
 
 module.exports.postcss = true
